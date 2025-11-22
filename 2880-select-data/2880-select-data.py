3c2814a1ec0b4a37f8b00c61c42885fd77ddcd4b
Anurag <anuragomer799@gmail.com>
2025-11-22 11:34:55 +0530

    Time: 307 ms (20.96%), Space: 67.4 MB (7.48%) - LeetHub

--- 2880-select-data/2880-select-data.py
+++ 2880-select-data/2880-select-data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
-    df=pd.DataFrame(students)
-    return df[['name','age']][df['student_id']==101]
+    # df=pd.DataFrame(students)
+    # return df[['name','age']][df['student_id']==101]
+
+    # return students[['name','age']][students['student_id']==101]
+
+    return students[students['student_id']==101][['name','age']]

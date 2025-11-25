a5cf75535041d25e1fe9ba2eb0a4a09fa0c82f58
Anurag <anuragomer799@gmail.com>
2025-11-25 07:38:04 +0530

    Time: 250 ms (89.3%), Space: 68.7 MB (75.6%) - LeetHub

--- 0595-big-countries/0595-big-countries.py
+++ 0595-big-countries/0595-big-countries.py
@@ -3,4 +3,7 @@ import pandas as pd
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     res=world[(world['area']>=3000000)|(world['population']>=25000000)][['name','population','area']]
     return res
+
+    # big_countries = world.loc[(world["area"] >= 3000000) | (world["population"] >= 25000000)]
+    # return big_countries.loc[:, ["name", "population", "area"]]
     

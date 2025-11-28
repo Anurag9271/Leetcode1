f9ade65c1fff9d70d428fc44e381e07d2ff86453
Anurag <anuragomer799@gmail.com>
2025-11-28 19:26:09 +0530

    Time: 292 ms (43.83%), Space: 67.9 MB (41.91%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    actor_director = actor_director.groupby(['actor_id','director_id'])['timestamp'].size().reset_index()
+    return actor_director[actor_director['timestamp']>=3][['actor_id','director_id']]

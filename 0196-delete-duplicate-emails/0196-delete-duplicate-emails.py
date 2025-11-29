db89122dc84521800254c7d738d028cca8e7d223
Anurag <anuragomer799@gmail.com>
2025-11-29 07:36:34 +0530

    Time: 339 ms (5.02%), Space: 67.4 MB (58.36%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
     person.sort_values('id',ascending=True,inplace=True)
-    person = person.drop_duplicates(subset='email',keep='first',inplace=True)
-    return person
+    person.drop_duplicates(subset='email',keep='first',inplace=True)
+    
